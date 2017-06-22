5bdc2b978fb8af9435c08d8528a48a7e759e9e9e
sanpsicro <sanpsicro@hotmail.com>
2017-06-22 01:25:55 -0500

    contratos.php

--- contratos.php
+++ contratos.php
@@ -9,6 +9,18 @@ $quest = $_GET['quest'];
 if(empty($show)){$show=10;}
 if(empty($sort)){$sort="numPoliza";}
 if(empty($status)){$status="activo";}
+
+function mysqli_result($res,$row=0,$col=0){
+	$numrows = mysqli_num_rows($res);
+	if ($numrows && $row <= ($numrows-1) && $row >=0){
+		mysqli_data_seek($res,$row);
+		$resrow = (is_numeric($col)) ? mysqli_fetch_row($res) : mysqli_fetch_assoc($res);
+		if (isset($resrow[$col])){
+			return $resrow[$col];
+		}
+	}
+	return false;
+}
 ?>
  <script type="text/javascript" language="JavaScript">
 function confirmGeneral(generalurl) { 
